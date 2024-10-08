1d964fd98053252c10c66e00c1d80ae8010868ba
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-08 22:37:39 +0100

    add path definitions

---
+++ production-deployer/src/definitions.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+source_root_dir = Path(__file__).resolve().parent  # <repo>/production-deployer/src
+project_root_dir = source_root_dir.parent  # <repo>/production-deployer
