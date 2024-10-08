70a2ba5ea752898f1c044043b9f76462c031af01
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-08 22:37:51 +0100

    create database connection

---
+++ production-deployer/src/storage.py
@@ -0,0 +1,24 @@
+from os import environ as os_environ
+from dotenv import dotenv_values
+
+from sqlalchemy import String, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+environment = {
+    **os_environ,
+    **dotenv_values(),
+}
+
+class Base(DeclarativeBase):
+    pass
+
+class Event(Base):
+    __tablename__ = "event"
+    id: Mapped[str] = mapped_column(String(36), primary_key=True)
+
+    def __repr__(self) -> str:
+        return f"Event(id={self.id!r})"
+
+engine = create_async_engine(environment["DATABASE_URL"], echo=True)
