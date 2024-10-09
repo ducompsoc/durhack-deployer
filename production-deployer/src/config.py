aa1a1b1a6ea82926802a2fed004d0419823bde98
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-09 01:46:27 +0100

    add some more configuration stuff

--- production-deployer/src/config.py
+++ production-deployer/src/config.py
@@ -1,12 +1,25 @@
 from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, PositiveInt, NonNegativeInt
 
 from config_loader import load_config
 from definitions import project_root_dir
 
 
+class ProxyFixConfig(BaseModel):
+    x_for: NonNegativeInt
+    x_proto: NonNegativeInt
+
+
+class ListenConfig(BaseModel):
+    host: str
+    port: PositiveInt
+
+
 class DeployerConfig(BaseModel):
+    listen: ListenConfig
+    proxy_fix: ProxyFixConfig
+    origin: HttpUrl
     webhook_secret_token: str
 
 
