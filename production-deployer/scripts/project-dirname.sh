0164ed3268ca3d8082d317fb2d1f878033c1c555
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-15 13:18:09 +0100

    split `main-worker.sh` into smaller scriptlets

--- production-deployer/scripts/project-dirname.sh
+++ production-deployer/scripts/project-dirname.sh
@@ -2,7 +2,5 @@
 
 # https://stackoverflow.com/a/77663806/11045433
 SCRIPTS_DIRNAME=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
-
-source "$SCRIPTS_DIRNAME"/activate-environment.sh
-
-python -m main_queue_worker.app
+PROJECT_DIRNAME=$(dirname "$SCRIPTS_DIRNAME")
+export PROJECT_DIRNAME
