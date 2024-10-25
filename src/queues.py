5ec2817f0414a7ada4fd5cd73b02e6efd0aef036
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-25 14:21:16 +0100

    fix: remove `$` from queue item filenames

--- src/queues.py
+++ src/queues.py
@@ -18,7 +18,7 @@ class Queue:
 
     def push_event(self, event: object) -> None:
         self.path.mkdir(parents=True, exist_ok=True)
-        event_item_filepath = Path(self.path, f"${time_ns()}.json")
+        event_item_filepath = Path(self.path, f"{time_ns()}.json")
         with open(event_item_filepath, "x") as event_item_handle:
             durhack_deployer_json_dump(event, event_item_handle)
 
