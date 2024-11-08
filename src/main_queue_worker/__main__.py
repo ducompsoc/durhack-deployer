90bf34d4dca64a2107693fa5bd553f0d27417676
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-08 12:46:48 +0000

    we do a little refactoring (CLI args!)

--- src/main_queue_worker/__main__.py
+++ src/main_queue_worker/__main__.py
@@ -1,6 +1,22 @@
 import asyncio
+import argparse
 
-from .worker import main
+from queue_worker_base import run_worker
+from queues import main_queue
+from .worker import MainQueueWorker
+
+
+parser = argparse.ArgumentParser(
+    prog="main-queue-worker",
+    description="Handles queued 'push' events from various DurHack repositories by piping them to the appropriate worker queue(s).",
+)
+
+
+async def main() -> None:
+    parser.parse_args()
+    main_queue.path.mkdir(parents=True, exist_ok=True)
+    await run_worker(MainQueueWorker, main_queue)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
