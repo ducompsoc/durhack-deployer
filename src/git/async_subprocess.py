242aef42d9e2f58769c852fdc12f2e8ce22f5197
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-09 00:23:54 +0000

    add util for invoking `git fetch`

---
+++ src/git/async_subprocess.py
@@ -0,0 +1,44 @@
+import asyncio
+from dataclasses import dataclass
+import functools
+import os
+
+
+@dataclass
+class SubprocessResult:
+    exit_code: int
+    stdout: str
+    stderr: str
+
+
+@functools.cache
+def set_env() -> dict:
+    """
+    GIT_TERMINAL_PROMPT=0 disallows spurious Git https password prompts
+    https://github.blog/2015-02-06-git-2-3-has-been-released/#the-credential-subsystem-is-now-friendlier-to-scripting
+    """
+
+    env = os.environ.copy()
+    env["GIT_TERMINAL_PROMPT"] = "0"
+    return env
+
+
+async def run(cmd: str) -> SubprocessResult:
+    env = set_env()
+
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        env=env,
+    )
+    stdout, stderr = await proc.communicate()
+
+    if (code := proc.returncode) is None:
+        code = 1
+
+    return SubprocessResult(
+        code,
+        stdout.decode("utf8", errors="ignore").rstrip(),
+        stderr.decode("utf8", errors="ignore").rstrip(),
+    )
---
+++ src/git/fetch.py
@@ -0,0 +1,24 @@
+import asyncio
+from logging import Logger, getLogger
+from pathlib import Path
+
+import async_subprocess
+
+
+async def fetch(path: Path, logger: Logger | None = None) -> None:
+    logger = logger if logger is not None else getLogger(__name__)
+
+    result = await async_subprocess.run(
+        f"git -C {path} fetch"
+    )
+
+    if result.exit_code == 0:
+        return
+
+    if "Permission denied" in result.stderr or "fatal: could not read Password" in result.stderr:
+        raise Exception(f"`git fetch` exited with status {result.exit_code}; credentials needed for {path.name}: {result.stderr}")
+
+    raise Exception(f"`git fetch` exited with status {result.exit_code}; {result.stderr}")
+
+if __name__ == "__main__":
+    asyncio.run(fetch(Path("/", "home", "joeclack", "PycharmProjects", "durhack-deployer")))
