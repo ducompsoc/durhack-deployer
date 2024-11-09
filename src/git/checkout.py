70ecb5489ca74d2232bece80e006b35acc86c893
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-09 00:27:32 +0000

    add util for invoking `git checkout`

---
+++ src/git/checkout.py
@@ -0,0 +1,22 @@
+import asyncio
+from logging import Logger, getLogger
+from pathlib import Path
+
+import async_subprocess
+
+
+async def checkout(path: Path, commit_hash: str, logger: Logger | None = None) -> None:
+    logger = logger if logger is not None else getLogger(__name__)
+
+    result = await async_subprocess.run(
+        f"git -C {path} checkout {commit_hash}"
+    )
+
+    if result.exit_code == 0:
+        return
+
+    raise Exception(f"`git checkout {commit_hash}` exited with status {result.exit_code}; {result.stderr}")
+
+
+if __name__ == "__main__":
+    asyncio.run(checkout(Path("/", "home", "joeclack", "PycharmProjects", "durhack-deployer"), "bd0486a9501ddb765c853b8e93bfd7c4766bf95b"))
