e68466c76c30c9d0c04b51ea1e50edb0b832a455
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-11 14:14:38 +0000

    fix: remove `aggregate_commit_files` from `util.__init__`

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,12 +1,9 @@
-from .aggregate_commit_files import __all__ as aggregate_commit_files_all
 from .configure_console_logging import __all__ as configure_console_logging_all
 
 __all__ = (
-    *aggregate_commit_files_all,
     *configure_console_logging_all,
     "async_subprocess",
 )
 
-from .aggregate_commit_files import *
 from .configure_console_logging import *
 from . import async_subprocess
