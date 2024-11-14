86fce86d93e181aae5d226949bda258269732856
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-14 17:34:19 +0000

    support specifying list of signals to listen for

--- src/util/async_interrupt.py
+++ src/util/async_interrupt.py
@@ -2,14 +2,20 @@ import asyncio
 import signal
 
 
-def create_interrupt_future(loop: asyncio.AbstractEventLoop) -> asyncio.Future[None]:
+def create_interrupt_future(
+    loop: asyncio.AbstractEventLoop,
+    signals: list[signal.Signals] | None = None,
+) -> asyncio.Future[None]:
     interrupted = loop.create_future()
+
+    if signals is None:
+        signals = [signal.SIGINT, signal.SIGTERM]
 
     def on_interrupt(signal_index: int):
         if interrupted.done():
             return
         interrupted.set_result(None)
 
-    loop.add_signal_handler(signal.SIGINT, on_interrupt, signal.SIGINT)
-    loop.add_signal_handler(signal.SIGTERM, on_interrupt, signal.SIGTERM)
+    for signal_index in signals:
+        loop.add_signal_handler(signal_index, on_interrupt, signal_index)
     return interrupted
