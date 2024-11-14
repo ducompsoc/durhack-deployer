3c51a19631f46163c667f7996253463901b435a0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-14 12:14:48 +0000

    add `shell.pipenv` module

---
+++ src/shell/pipenv/install.py
@@ -0,0 +1,21 @@
+from logging import Logger, getLogger
+from pathlib import Path
+
+from util import async_subprocess
+
+
+async def install(
+    path: Path,
+    logger: Logger | None = None,
+) -> None:
+    logger = logger if logger is not None else getLogger(__name__)
+
+    result = await async_subprocess.run(
+        f"pipenv install --deploy --dev",
+        cwd=path,
+    )
+
+    if result.exit_code <= 0:
+        return
+
+    raise Exception(f"`pipenv install` exited with status {result.exit_code}; {result.stderr}")
