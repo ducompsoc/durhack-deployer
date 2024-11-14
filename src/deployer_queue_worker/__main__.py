d71d1e58d1e340f43e7b55e1135bf68b76a9183f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-14 11:39:06 +0000

    add `deploy` subcommand to deployer queue worker

--- src/deployer_queue_worker/__main__.py
+++ src/deployer_queue_worker/__main__.py
@@ -1,6 +1,7 @@
 import asyncio
 import argparse
 from argparse import ArgumentTypeError
+from typing import Callable, Self, Awaitable
 
 from config import DeployerDeploymentConfig
 from deployments import lookup_deployment_by_slug, Deployment
@@ -12,6 +13,7 @@ parser = argparse.ArgumentParser(
     prog="deployer-queue-worker",
     description="Handles queued 'push' events from the ducompsoc/durhack-deployer repository.",
 )
+
 
 def lookup_deployer_deployment_by_slug(slug: str) -> Deployment[DeployerDeploymentConfig]:
     deployment = lookup_deployment_by_slug(slug)
@@ -31,16 +33,36 @@ parser.add_argument(
     metavar="slug",
     type=lookup_deployer_deployment_by_slug,
 )
+subparsers = parser.add_subparsers(required=False)
+
+run_parser = subparsers.add_parser("run")
+
+deploy_parser = subparsers.add_parser("deploy")
 
 
 class DeployerArgNamespace(argparse.Namespace):
     deployment: Deployment[DeployerDeploymentConfig]
+    main: Callable[[Self], Awaitable[None]]
+
+
+async def run(args: DeployerArgNamespace) -> None:
+    args.deployment.queue.path.mkdir(parents=True, exist_ok=True)
+    await run_worker(DeployerQueueWorker, args.deployment)
+
+
+async def deploy(args: DeployerArgNamespace) -> None:
+    worker = DeployerQueueWorker(args.deployment)
+    await worker.on_init()
+
+
+parser.set_defaults(main=run)
+run_parser.set_defaults(main=run)
+deploy_parser.set_defaults(main=deploy)
 
 
 async def main() -> None:
     args = parser.parse_args(None, DeployerArgNamespace())
-    args.deployment.queue.path.mkdir(parents=True, exist_ok=True)
-    await run_worker(DeployerQueueWorker, args.deployment)
+    await args.main(args)
 
 
 if __name__ == '__main__':
--- src/deployer_queue_worker/worker.py
+++ src/deployer_queue_worker/worker.py
@@ -15,5 +15,12 @@ class DeployerQueueWorker(GitHubRepositoryQueueWorker):
     @override
     async def on_push(self, payload: PushEvent) -> None:
         await self.checkout(payload["head_commit"]["id"])
+        await self.deploy()
+
+    async def on_init(self) -> None:
+        await self.deploy()
+
+    async def deploy(self) -> None:
+        # todo: pipenv install
         await uwsgi.reload(self.config.uwsgi_config_path, self._logger)
         await systemctl.restart(self.config.systemd_unit_name, block=False, logger=self._logger)
