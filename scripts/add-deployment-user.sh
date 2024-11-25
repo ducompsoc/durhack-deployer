80e49c5f30d4c2ffea954c56b7de50f8ac115015
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-25 18:10:00 +0000

    fix: use `trap` to ensure modified options are restored on exit

--- scripts/add-deployment-user.sh
+++ scripts/add-deployment-user.sh
@@ -8,13 +8,12 @@ fi
 
 # script should exit if any command fails
 grep -F e <<< "$-"; errexit_unset=$?
-((errexit_unset)) && set -o errexit
+((errexit_unset)) \
+  && set -o errexit \
+  && trap "set +o errexit" RETURN
 
 # create the user account
 adduser "$1" --disabled-password --home /home/"$1" --gecos ""
 
 # enable the `ssh-agent` user-service on the new account
 systemd-run --wait --collect --user --machine="$1"@ systemctl enable --user ssh-agent
-
-# restore unmodified options
-((errexit_unset)) && set +o errexit
--- scripts/deployment-user-self-setup.sh
+++ scripts/deployment-user-self-setup.sh
@@ -10,13 +10,13 @@ cat <<EOF > "$NVM_BASHRC"
 # ~/.bashrc.d/nvm.bashrc
 
 grep -F a <<< "$-"; allexport_unset=$?
-((allexport_unset)) && set -o allexport
+((allexport_unset)) \
+  && set -o allexport \
+  && trap "set +o allexport" RETURN
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-((allexport_unset)) && set +o allexport
 EOF
 # setup nvm end
 
