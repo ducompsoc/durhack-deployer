0bf88aa893f76468d3b8a43e94ac2e6736feb5be
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-25 18:07:07 +0000

    `--gecos`, not `--comment`

--- scripts/add-deployment-user.sh
+++ scripts/add-deployment-user.sh
@@ -11,7 +11,7 @@ grep -F e <<< "$-"; errexit_unset=$?
 ((errexit_unset)) && set -o errexit
 
 # create the user account
-adduser "$1" --disabled-password --home /home/"$1" --comment ""
+adduser "$1" --disabled-password --home /home/"$1" --gecos ""
 
 # enable the `ssh-agent` user-service on the new account
 systemd-run --wait --collect --user --machine="$1"@ systemctl enable --user ssh-agent
