415ea485f11bf2316f8311ade753c67794551a87
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-25 17:57:32 +0000

    exit script if any command fails

--- scripts/add-deployment-user.sh
+++ scripts/add-deployment-user.sh
@@ -6,8 +6,15 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+# script should exit if any command fails
+grep -F e <<< "$-"; errexit_unset=$?
+((errexit_unset)) && set -o errexit
+
 # create the user account
 adduser "$1" --disabled-password --home /home/"$1" --comment ""
 
 # enable the `ssh-agent` user-service on the new account
 systemd-run --wait --collect --user --machine="$1"@ systemctl enable --user ssh-agent
+
+# restore unmodified options
+((errexit_unset)) && set +o errexit
