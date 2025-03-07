6c7e804e5bffe72b6c44950dd7b45519b779c7fe
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-07 17:26:29 +0000

    add scripts suitable for replacing Pipfile scripts

---
+++ scripts/migrate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://stackoverflow.com/a/77663806/11045433
+SCRIPTS_DIRNAME=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
+
+source "$SCRIPTS_DIRNAME"/activate-environment.sh
+
+alembic upgrade head
