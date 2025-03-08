2a4661f992827761ed5faec75843211c9dafb970
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-08 01:08:08 +0000

    rename `project-dirname.sh` to `definitions.sh`

--- scripts/activate-environment.sh
+++ scripts/activate-environment.sh
@@ -3,7 +3,7 @@
 # https://stackoverflow.com/a/77663806/11045433
 SCRIPTS_DIRNAME=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
 
-source "$SCRIPTS_DIRNAME"/project-dirname.sh
+source "$SCRIPTS_DIRNAME"/definitions.sh
 source "$PROJECT_DIRNAME"/.venv/bin/activate
 PYTHONPATH="$PROJECT_DIRNAME"/src:"$PYTHONPATH"
 export PYTHONPATH
--- scripts/gosmee-dev.sh
+++ scripts/gosmee-dev.sh
@@ -3,7 +3,7 @@
 # https://stackoverflow.com/a/77663806/11045433
 SCRIPTS_DIRNAME=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
 
-source "$SCRIPTS_DIRNAME"/project-dirname.sh
+source "$SCRIPTS_DIRNAME"/definitions.sh
 source "$PROJECT_DIRNAME"/.env
 
 if [ -z "$WEBHOOK_PROXY_URL" ]; then
