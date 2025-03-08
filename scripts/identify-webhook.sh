6b7ab0556759dd22717f340e07ac3da29e30c7ba
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-08 01:30:44 +0000

    prefer `source ... definitions.sh` to copy+paste

--- scripts/identify-webhook.sh
+++ scripts/identify-webhook.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-REPOSITORY_FULL_NAME="ducompsoc/durhack-deployer"
-declare -r REPOSITORY_FULL_NAME
-export REPOSITORY_FULL_NAME
+# https://stackoverflow.com/a/77663806/11045433
+SCRIPTS_DIRNAME=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
+
+source "$SCRIPTS_DIRNAME"/definitions.sh
 
 # shellcheck disable=SC2155 # Intended dynamic declaration
 declare -A hook_options="($(
