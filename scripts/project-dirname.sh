af0369197245d64032b8ddb3a9c7b1562351e6d9
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-08 01:06:54 +0000

    make exported `PROJECT_DIRNAME` readonly

--- scripts/project-dirname.sh
+++ scripts/project-dirname.sh
@@ -3,4 +3,5 @@
 # https://stackoverflow.com/a/77663806/11045433
 SCRIPTS_DIRNAME=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
 PROJECT_DIRNAME=$(dirname "$SCRIPTS_DIRNAME")
+declare -r PROJECT_DIRNAME
 export PROJECT_DIRNAME
