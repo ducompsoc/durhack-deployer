37fecfa5d6390a9db9d35b0448d0c3e19116ba07
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-06-19 08:25:07 +0100

    accept `env`, `cwd` params to `pnpm` functions, fix #12

--- src/shell/pnpm/run.py
+++ src/shell/pnpm/run.py
@@ -3,13 +3,26 @@ from pathlib import Path
 
 from util import async_subprocess
 
-async def run(project_path: Path, script: str, filter_selector: str | None = None, logger: Logger | None = None) -> None:
+
+async def run(
+    project_path: Path,
+    script: str,
+    filter_selector: str | None = None,
+    env: dict[str, str] | None = None,
+    cwd: Path | None = None,
+    logger: Logger | None = None,
+) -> None:
     logger = logger if logger is not None else getLogger(__name__)
+    if cwd is not None:
+        raise ValueError("cwd must be None for pnpm commands: https://github.com/ducompsoc/durhack-deployer/issues/12")
+    cwd = project_path
 
     filter_option = "" if filter_selector is None else f"--filter '{filter_selector}'"
 
     result = await async_subprocess.run(
         f"pnpm -C '{project_path}' {filter_option} run '{script}'",
+        env=env,
+        cwd=cwd,
     )
 
     if result.exit_code <= 0:
